0093b8a10aff6dd9b18f735fad23855ba30a346b
AlexKa <x182838@gmail.com>
2019-09-21 16:04:18 +0300

    mobile menu

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-svg4everybody();
+$('.header__hamburger').click(function() {
+
+  $('.header__m-search').closest('.header__wrap-m-box').fadeOut();
+
+  if($('.header__m-box-nav').closest('.header__wrap-m-box').css('display') === 'none') {
+    $('.header__m-box-nav').closest('.header__wrap-m-box').slideDown();
+    $('body').css("overflow-y", "hidden");
+  } else {
+    $('.header__m-box-nav').closest('.header__wrap-m-box').slideUp();
+    $('body').css("overflow-y", "auto");
+  }
+
+})
+
+$('.js.search__btn').click(function() {
+  $('.header__m-box-nav').closest('.header__wrap-m-box').fadeOut();
+
+  if($('.header__m-search').closest('.header__wrap-m-box').css('display') === 'none') {
+    $('.header__m-search').closest('.header__wrap-m-box').slideDown();
+    $('body').css("overflow-y", "hidden");
+  } else {
+    $('.header__m-search').closest('.header__wrap-m-box').slideUp();
+    $('body').css("overflow-y", "auto");
+  }
+
+})
