f42329c1aeeb49aa6bf83b23fe322f4457fcb4c8
AlexKa <x182838@gmail.com>
2019-10-03 10:30:23 +0300

    page exam2

--- gulpfile.js
+++ gulpfile.js
@@ -99,6 +99,7 @@ gulp.task('js:libs', function () {
       'node_modules/svg4everybody/dist/svg4everybody.min.js',
       'node_modules/slick-carousel/slick/slick.min.js',
       'node_modules/selectric/public/jquery.selectric.min.js',
+      'node_modules/simplelightbox/dist/simple-lightbox.min.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -2,16 +2,16 @@
 
 let body = document.querySelector('body');
 
-$(window).resize(function() {
+$(window).resize(function () {
 
-  if($('.header__search').css('display') !== 'none') {
+  if ($('.header__search').css('display') !== 'none') {
     $('.header__m-search').closest('.header__wrap-m-box').hide();
-    if($('.header__m-box-nav').closest('.header__wrap-m-box').css('display') === 'none') {
+    if ($('.header__m-box-nav').closest('.header__wrap-m-box').css('display') === 'none') {
       body.style.overflowY = 'auto';
     }
   }
 
-  if($('.header__box-nav').css('display') !== 'none') {
+  if ($('.header__box-nav').css('display') !== 'none') {
     $('.header__m-box-nav').closest('.header__wrap-m-box').hide();
     body.style.overflowY = 'auto';
   }
@@ -19,11 +19,11 @@ $(window).resize(function() {
 })
 
 // =============================================== header nav
-$('.js.header__hamburger').click(function() {
+$('.js.header__hamburger').click(function () {
 
   $('.header__m-search').closest('.header__wrap-m-box').hide();
 
-  if($('.header__m-box-nav').closest('.header__wrap-m-box').css('display') === 'none') {
+  if ($('.header__m-box-nav').closest('.header__wrap-m-box').css('display') === 'none') {
     $('.header__m-box-nav').closest('.header__wrap-m-box').slideDown();
     body.style.overflowY = 'hidden';
   } else {
@@ -32,10 +32,10 @@ $('.js.header__hamburger').click(function() {
   }
 })
 
-$('.js.search__btn').click(function() {
+$('.js.search__btn').click(function () {
   $('.header__m-box-nav').closest('.header__wrap-m-box').hide();
 
-  if($('.header__m-search').closest('.header__wrap-m-box').css('display') === 'none') {
+  if ($('.header__m-search').closest('.header__wrap-m-box').css('display') === 'none') {
     $('.header__m-search').closest('.header__wrap-m-box').slideDown();
     $('body').css("overflow-y", "hidden");
   } else {
@@ -48,19 +48,19 @@ $('.js.search__btn').click(function() {
 // =================================================== ddmenu
 let timerCloseDDmenu;
 
-$('.main-nav__link.dropdown').mouseenter(function() {
+$('.main-nav__link.dropdown').mouseenter(function () {
   $('.main-nav__wrap-ddmenu').slideDown(200);
 })
 
-$('.main-nav__link.dropdown').mouseleave(function() {
+$('.main-nav__link.dropdown').mouseleave(function () {
   timerCloseDDmenu = setTimeout(closeDDmenu, 150);
 })
 
-$('.main-nav__wrap-ddmenu').mouseenter(function() {
+$('.main-nav__wrap-ddmenu').mouseenter(function () {
   clearTimeout(timerCloseDDmenu);
 })
 
-$('.main-nav__wrap-ddmenu').mouseleave(function() {
+$('.main-nav__wrap-ddmenu').mouseleave(function () {
   closeDDmenu();
 })
 
@@ -71,7 +71,7 @@ function closeDDmenu() {
 
 
 // ======================================================= tab
-$('.tab__btn').on('click', function() {
+$('.tab__btn').on('click', function () {
   let id = $('.tab__btn').index(this);
 
   $('.tab__btn').removeClass('active');
@@ -81,19 +81,42 @@ $('.tab__btn').on('click', function() {
   $('.tab-content__item').eq(id).fadeIn();
 })
 
+// ======================================================== changes the price
+const exam1Checkbox = document.querySelector('#exam1__checkbox');
+const exam1PlusElement = document.querySelector('.exam1__box-input-label .ex-pr');
+const exam1OldPrice = document.querySelector('.exam-add-cart__old-price .ex-pr');
+const exam1NewPrice = document.querySelector('.exam-add-cart__new-price .ex-pr');
+
+if (exam1Checkbox) {
+  exam1Checkbox.addEventListener('click', function () {
+    if (this.checked) {
+      exam1OldPrice.textContent = (+exam1OldPrice.textContent + +exam1PlusElement.textContent).toFixed(2);
+      exam1NewPrice.textContent = (+exam1NewPrice.textContent + +exam1PlusElement.textContent).toFixed(2);
+    } else {
+      exam1OldPrice.textContent = (+exam1OldPrice.textContent - +exam1PlusElement.textContent).toFixed(2);
+      exam1NewPrice.textContent = (+exam1NewPrice.textContent - +exam1PlusElement.textContent).toFixed(2);
+    }
+  })
+}
 
 // ======================================================== add cart
-$('.exam-add-cart__btn').on('click', function() {
+$('.exam-add-cart__btn').on('click', function () {
   $('.exam-add-cart__wrap-response').fadeIn();
+})
+
+// ======================================================== add cart purchase
+$('.purchase__add-to-cart').on('click', function () {
+  $(this).closest('.purchase__box-add-to-cart').find('.purchase__wrap-response').fadeIn();
+  $(this).hide();
 })
 
 
 // ========================================================== slick
-$('.slider__arrow._prev').on('click', function() {
+$('.slider__arrow._prev').on('click', function () {
   $('.slick-prev').click();
 })
 
-$('.slider__arrow._next').on('click', function() {
+$('.slider__arrow._next').on('click', function () {
   $('.slick-next').click();
 })
 
@@ -104,74 +127,49 @@ $('.slider__list').slick({
   focusOnSelect: true,
   dots: true,
   appendDots: $('.slider__dots'),
-  responsive: [
-    {
-      breakpoint: 576,
-      settings: {
-        variableWidth: false,
-      }
+  responsive: [{
+    breakpoint: 576,
+    settings: {
+      variableWidth: false,
     }
-  ]
+  }]
 });
 
-// slider-v2
-
+// =============================================================== slider-v2
 $('.slider-v2__list').slick({
   slidesToShow: 4,
   infinite: false,
 });
 
-$('.slider-v2__btn.__prev').on('click', function() {
+checkDisabledArrow();
+
+function checkDisabledArrow() {
+  if($('.slick-prev').hasClass('slick-disabled')) {
+    $('.slider-v2__btn.__prev').css({'opacity': '0', 'cursor': 'default'});
+  } else {
+    $('.slider-v2__btn.__prev').css({'opacity': '1', 'cursor': 'pointer'});
+  }
+
+  if($('.slick-next').hasClass('slick-disabled')) {
+    $('.slider-v2__btn.__next').css({'opacity': '0', 'cursor': 'default'});
+  } else {
+    $('.slider-v2__btn.__next').css({'opacity': '1', 'cursor': 'pointer'});
+  }
+}
+
+$('.slider-v2__btn.__prev').on('click', function () {
   $('.slick-prev').click();
+  checkDisabledArrow();
 })
 
-$('.slider-v2__btn.__next').on('click', function() {
+$('.slider-v2__btn.__next').on('click', function () {
   $('.slick-next').click();
+  checkDisabledArrow();
 })
-
-
-/*REMOVE or comment below code to see behavior of infinite false setting below code handles removing of arrows*/
-$('.slider-v2__list').on('afterChange', function(){
-  console.log($('#slider').slick('slickCurrentSlide'));
-
-//   var currentSlide = $('#slider').slick('slickCurrentSlide');
-// if(currentSlide==0)
-// {
-//    $('.slick-prev').hide();
-//    $('.slick-next').show();
-// }
-// else if(currentSlide==5)
-// {
-// $('.slick-next').hide();
-// $('.slick-prev').show();
-// }
-
-// if(currentSlide>0 && currentSlide<5)
-// {
-//    $('.slick-prev').show();
-//    $('.slick-next').show();
-// }
-
-// });
-
-// $(document).ready(function(){
-// var currentSlide = $('#slider').slick('slickCurrentSlide');
-// if(currentSlide==0)
-// {
-//    $('.slick-prev').hide();
-// }
-// else if(currentSlide==5)
-// {
-// $('.slick-next').hide();
-// }
-
-});
-
 
 // ================================================================= tab-exam2__more
-$('.tab-exam2__more').on('click', function() {
-
-  if($(this).text() == 'More...') {
+$('.tab-exam2__more').on('click', function () {
+  if ($(this).text() == 'More...') {
     $('.tab-exam2__desc-p').fadeIn();
     $(this).text('Close');
   } else {
@@ -179,3 +177,8 @@ $('.tab-exam2__more').on('click', function() {
     $(this).text('More...');
   }
 })
+
+// =================================================================== lightbox
+if(document.querySelector('.slider-v2__item')) {
+  $('.slider-v2__item').simpleLightbox();
+}
